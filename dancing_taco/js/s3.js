c61d13de085b45d6eef7f8ff1dbb87f22b4afbc4
Hyein93 <dlsl5418@gmail.com>
2024-09-21 01:19:14 +0900

    0921

---
+++ dancing_taco/js/s3.js
@@ -0,0 +1,13 @@
+
+$(function () {
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        minSlides: 3,
+        maxSlides: 3,
+        moveSlides: 1,
+        slideWidth: 358,
+        slideMargin: 42
+    });
+});
