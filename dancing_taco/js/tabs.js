3b6d82c41f991c86ce7c1503e851866aed7e73c8
Hyein93 <dlsl5418@gmail.com>
2024-09-26 18:49:17 +0900

    0926

--- dancing_taco/js/tabs.js
+++ dancing_taco/js/tabs.js
@@ -1,40 +1,41 @@
-// section2 - tabs
-// Vanilla JS
-
-window.addEventListener("DOMContentLoaded", function () {
-
+$(() => {
     // 요소 찾기
-    const btn1 = document.querySelectorAll('#tabs a').item(0);
-    const btn2 = document.querySelectorAll('#tabs a').item(1);
-    const btn3 = document.querySelectorAll('#tabs a').item(2);
-    const btn4 = document.querySelectorAll('#tabs a').item(3);
-    // console.log(btn1,btn2,btn3,btn4);
+    const btn1 = document.querySelectorAll('#tabs li').item(0);
+    const btn2 = document.querySelectorAll('#tabs li').item(1);
+    const btn3 = document.querySelectorAll('#tabs li').item(2);
+    const btn4 = document.querySelectorAll('#tabs li').item(3);
 
     const cont1 = document.querySelector('#tab1');
-    // console.log(cont1);
     const cont2 = document.querySelector('#tab2');
     const cont3 = document.querySelector('#tab3');
     const cont4 = document.querySelector('#tab4');
 
-    // 이벤트 처리
-    btn1.onclick = function (e) {
-        e.preventDefault();
+    btn1.onclick = function () {
         resetF();
+        resetC();
+        btn1.classList.add('text');
         cont1.classList.add('on');
     }
-    btn2.onclick = function (e) {
-        e.preventDefault();
+
+
+    btn2.onclick = function () {
         resetF();
+        resetC();
+        btn2.classList.add('text');
         cont2.classList.add('on');
     }
-    btn3.onclick = function (e) {
-        e.preventDefault();
+
+    btn3.onclick = function () {
         resetF();
+        resetC();
+        btn3.classList.add('text');
         cont3.classList.add('on');
     }
-    btn4.onclick = function (e) {
-        e.preventDefault();
+
+    btn4.onclick = function () {
         resetF();
+        resetC();
+        btn4.classList.add('text');
         cont4.classList.add('on');
     }
 
@@ -44,5 +45,10 @@ window.addEventListener("DOMContentLoaded", function () {
         cont3.classList.remove('on');
         cont4.classList.remove('on');
     }
-
-});
+    function resetC() {
+        btn1.classList.remove('text');
+        btn2.classList.remove('text');
+        btn3.classList.remove('text');
+        btn4.classList.remove('text');
+    }
+}); // ready end
